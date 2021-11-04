b281a59d0015e2acfd2864387d07b23ffa969f0b
Benjamin Marin <b.marin@student.isartdigital.com>
2021-11-04 10:47:13 +0100

    LifeComponent fix, Starter conter disabled

--- Source/GladiatorGame/LifeComponent.cpp
+++ Source/GladiatorGame/LifeComponent.cpp
@@ -4,7 +4,8 @@
 #include "LifeComponent.h"
 
 // Sets default values for this component's properties
-ULifeComponent::ULifeComponent()
+ULifeComponent::ULifeComponent(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
