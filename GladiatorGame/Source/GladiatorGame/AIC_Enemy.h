573b1203fbda3be67f380c6bad9178348dad4b2a
Benjamin Marin <b.marin@student.isartdigital.com>
2021-11-04 16:58:42 +0100

    Enemy Movements AI (WIP)

---
+++ GladiatorGame/Source/GladiatorGame/AIC_Enemy.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "AIC_Enemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GLADIATORGAME_API AAIC_Enemy : public AAIController
+{
+	GENERATED_BODY()
+	
+public :
+	AAIC_Enemy();
+};
